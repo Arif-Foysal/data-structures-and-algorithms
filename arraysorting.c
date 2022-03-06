c342e19fa98af255648ba587ac991d9e8bd0a3da
Parrot Live user <user@localhost.localdomain>
2022-03-06 16:32:25 +0000

    first commit

---
+++ arraysorting.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void printarray(int array[], int size);
+void insertionSort(int array[], int size);
+void main(){
+printf("Size of the array?\n");
+int n;
+scanf("%d",&n);
+int numbers[n];
+for(int i=0;i<n;i++){
+printf("Enter num %d: ",i+1);
+scanf("%d",&numbers[i]);
+}
+printarray(numbers, n);
+insertionSort(numbers, n);
+printf("After sorting:\n");
+printarray(numbers,n);
+}
+void printarray(int array[], int size){
+for (int i=0; i<size; i++) {
+printf("%d ",array[i]);
+}
+printf("\n");
+}
+//Insertion sort
+void insertionSort(int array[], int size){
+for (int i=1;i<size; i++) {
+int current=array[i];
+int j=i-1;
+while (current<array[j] && j>=0 ){
+array[j+1]=array[j];
+j--;
+}
+array[j+1]=current;
+}
+}
